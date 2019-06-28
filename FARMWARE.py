5e1fd99d6ed1a1bb5052acea1b2b195616a4e096
Neobuild <stagiaire@neobuild.lu>
2019-06-28 09:10:27 +0200

    Update FARMWARE.py

--- FARMWARE.py
+++ FARMWARE.py
@@ -477,11 +477,8 @@ class MyFarmware():
         log("Data loaded.", message_type='info')		#Just some fancy information.
 
         self.goto(0,0,0)					#send the bot to 0,0,0. Not necessary, but a nice check to see if the origin is properly set.
-
-        pause.until(datetime(2019, 6, 28, 9, 5))
-        self.goto(500,500,0)
-        pause.until(datetime(2019, 6, 28, 8,8))
-        self.goto(0,0,0)                               # try pause and move
+        time.sleep(500)
+        self.goto(500,0,0)                               # try pause and move
         self.water()						#Water sequence at line 525
         self.plant()                                            #Plant sequence at line 561
 
